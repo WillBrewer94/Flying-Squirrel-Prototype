ee72f03c8e05e70e9076feebe762947de5e5480c
Mike Adams <madams75@gatech.edu>
2018-04-13 15:51:30 -0400

    Added UI and damaging from chicken

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+    public RawImage hc1;
+    public RawImage hc2;
+    public RawImage hc0;
+    public Texture empty;
+    public Texture full;
+    int health = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.anyKeyDown)
+        //{
+            //lowerHealth();
+        //}
+	}
+
+    public void lowerHealth()
+    {
+        if (health == 3)
+        {
+            hc2.texture = empty;
+        } else if (health == 2)
+        {
+            hc1.texture = empty;
+        } else
+        {
+            hc0.texture = empty;
+        }
+        health--;
+    }
+    public void raiseHealth()
+    {
+        if (health == 2)
+        {
+            hc2.texture = full;
+        }
+        else if (health == 1)
+        {
+            hc1.texture = full;
+        }
+        health++;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    int health = 3;
+    public HealthController healthController;
+    public float lastHit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter");
+        if (other.tag == "Enemy")
+        {
+            Debug.Log("enemy");
+            if (Time.time > lastHit + 2)
+            {
+                Debug.Log("health");
+                healthController.lowerHealth();
+                health--;
+                lastHit = Time.time;
+            }
+        }
+    }
+}
