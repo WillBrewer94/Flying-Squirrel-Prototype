038ded847a784f569ac4b5bf74b464d7521b2521
Patrick Black <patrickblack154@gmail.com>
2018-04-30 22:18:26 -0400

    Fixed floatiness of the air currents
    air currents should now be more smooth, but it is easy to edit the values

--- Assets/Scripts/BouncyCloud.cs
+++ Assets/Scripts/BouncyCloud.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class BouncyCloud : MonoBehaviour {
 
-	public float bounce = 50;
-	public float bounceStay = 5;
+	public float bounce = 70;
+	public float bounceStay = 20;
+	private float bounceOrig;
 
 	private Vector3 jumpVec;
 	private Vector3 jumpVecStay;
 
 	void Awake() {
 		jumpVec = new Vector3 (0, bounce, 0);
-		jumpVecStay = new Vector3 (0, bounceStay, 0);
+		bounceOrig = bounceStay;
 	}
 
+	/**
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("up");
@@ -23,14 +25,22 @@ public class BouncyCloud : MonoBehaviour {
 			p.SetVelocity(jumpVec);
 		}
     }
+    */
 
 
     void OnTriggerStay(Collider other)
     {
+		bounceStay+=3;
+		jumpVecStay = new Vector3 (0, bounceStay, 0);
 		Player p = other.attachedRigidbody.gameObject.GetComponent<Player>();
 		if (p != null) {
 			p.SetVelocity(jumpVecStay);
 		}
     }
+
+	void OnTriggerExit(Collider other)
+	{
+		bounceStay = bounceOrig;
+	}
     
 }
