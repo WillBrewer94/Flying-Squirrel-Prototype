96969942c819070712596c2b9a27d6ccfa34dd65
Mike Adams <madams75@gatech.edu>
2018-04-30 23:59:52 -0400

    Menu scene added with ability to launch game or quit. Dialog system functional for separate instances of NPC. Made the hub island bigger (in preparation to make branches off of it. Added skybox. CHANGED THE SCENES: "The Game" is the new game scene (it needed renaming). "The Menu" is the menu scene.

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void loadGame()
+    {
+        SceneManager.LoadScene("The Game");
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
